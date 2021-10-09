8a949e63b75fa3222c364eeb52c568af40524e6e
Moupiya-M67 <1905328@kiit.ac.in>
2021-10-10 00:59:28 +0530

    Sign Up changes
    css style change

--- client/src/auth/register.js
+++ client/src/auth/register.js
@@ -4,6 +4,9 @@ import { Link, Redirect } from "react-router-dom";
 import { setAlert } from "../actions/alert";
 import { register } from "../actions/auth";
 import PropTypes from "prop-types";
+import bgImage from './register.jpg';
+import './register.css'
+
 
 const Register = ({ setAlert, register, isAuthenticated }) => {
   const [formData, setFormData] = useState({
@@ -32,53 +35,62 @@ const Register = ({ setAlert, register, isAuthenticated }) => {
   }
 
   return (
-    <div style={{width: '50vw', margin: 'auto'}}>
-      <h1 className="large ">Sign Up</h1>
-      <p className="lead">
-        <i className="fas fa-user" /> Create Your Account
-      </p>
-      <form className="form" onSubmit={onSubmit}>
-        <div className="form-group">
-          <input
-            type="text"
-            placeholder="Name"
-            name="name"
-            value={name}
-            onChange={onChange}
-          />
-        </div>
-        <div className="form-group">
-          <input
-            type="email"
-            placeholder="Email Address"
-            name="email"
-            value={email}
-            onChange={onChange}
-          />
-        </div>
-        <div className="form-group">
-          <input
-            type="password"
-            placeholder="Password"
-            name="password"
-            value={password}
-            onChange={onChange}
-          />
-        </div>
-        <div className="form-group">
-          <input
-            type="password"
-            placeholder="Confirm Password"
-            name="password2"
-            value={password2}
-            onChange={onChange}
-          />
-        </div>
-        <input type="submit" className="btn" style={{backgroundColor: '#79d0f3', color: 'white', borderRadius: '35px'}} value="Register" />
-      </form>
-      <p className="my-1">
-        Already have an account? <Link to="/login" className=''>Sign In</Link>
-      </p>
+    <div style={{ display: 'flex', margin: '0px', backgroundColor: '#1d3536' }}>
+      <div style={{ width: '40%', margin: '50px', padding: '8px' }}>
+        <h1 className="large" style={{color: 'white'}} >Sign Up</h1>
+        <p className="lead" style={{color: 'white'}}>
+          <i className="fas fa-user" /> Create Your Account
+        </p>
+        <form className="form" onSubmit={onSubmit}>
+          <div className="form-group">
+            <input
+              type="text"
+              placeholder="Name"
+              name="name"
+              value={name}
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="email"
+              placeholder="Email Address"
+              name="email"
+              value={email}
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              value={password}
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              placeholder="Confirm Password"
+              name="password2"
+              value={password2}
+              onChange={onChange}
+            />
+          </div>
+          <input type="submit" className="btn" style={{ backgroundColor: '#d4373e', color: 'white', borderRadius: '35px' }} value="Register" />
+        </form>
+        <p className="my-1" style={{color: 'white'}}>
+          Already have an account? <Link to="/login" className='' style={{ color: '#d4373e'}}>Sign In</Link>
+        </p>
+      </div>
+      <div style={{
+        height: '100vh', width: '60%', padding: '8px', backgroundImage: `url(${bgImage})`, backgroundPosition: 'center',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat'
+      }}>
+
+      </div>
     </div>
   );
 };
