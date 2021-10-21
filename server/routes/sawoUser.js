520edaefe33c2a357cccebc0737d5167bf801026
sayantan1821 <ksayantan1823@gmail.com>
2021-10-22 02:55:38 +0530

    sawouser api bug fixed

--- server/routes/sawoUser.js
+++ server/routes/sawoUser.js
@@ -11,10 +11,17 @@ const cookieParser = require("cookie-parser");
 var app = express();
 app.use(cookieParser());
 
-router.get('/', auth, async (req, res) => {
- 
+router.get('/', [
+    check("name", "name is required").not().isEmpty(),
+    check("email", "Please enter a valid Email").isEmail(),
+    // check(
+    //     'password',
+    //     'Please enter a password with 6 or more characters'
+    // ).isLength({ min: 6 })
+  ], async (req, res) => {
+    const { name, email } = req.body;
     try {
-        const user = await User.findById(req.user.id).select('-password');
+        const user = await User.findOne({ email });
         res.json(user);
     } catch (err) {
         console.error(err.message);
@@ -44,9 +51,10 @@ router.post(
     try {
       let user = await User.findOne({ email });
       if (user) {
+          console.log("user Found ", user);
         const payload = {
           user: {
-            id: user.id,
+            id: user._id,
           },
         };
         jwt.sign(
@@ -58,44 +66,47 @@ router.post(
             res.json({ token });
           }
         );
+      } else {
+        const avatar = gravatar.url(email, {
+            s: "200",
+            r: "pg",
+            d: "mm",
+          });
+          user = new User({
+            name,
+            email,
+            avatar,
+            password,
+          });
+          const salt = await bcrypt.genSalt(10);
+          user.password = await bcrypt.hash(password, salt);
+          await user.save(function (err, user) {
+            if (err) console.log(err);
+            else {
+              console.log(user);
+              global.id = user._id; //global variable is declared to use the name and id in artists collection .
+              global.user_name = user.name;
+            }
+          });
+
+          const payload = {
+            user: {
+              id: user._id,
+            },
+          };
+          console.log(payload);
+          jwt.sign(
+            payload,
+            config.get("jwtSecret"),
+            { expiresIn: 360000 },
+            (err, token) => {
+              if (err) throw err;
+              res.json({ token });
+            }
+          );
       }
 
-      const avatar = gravatar.url(email, {
-        s: "200",
-        r: "pg",
-        d: "mm",
-      });
-      user = new User({
-        name,
-        email,
-        avatar,
-        password,
-      });
-      const salt = await bcrypt.genSalt(10);
-      user.password = await bcrypt.hash(password, salt);
-      await user.save(function (err, user) {
-        if (err) console.log(err);
-        else {
-          console.log(user);
-          global.id = user._id; //global variable is declared to use the name and id in artists collection .
-          global.user_name = user.name;
-        }
-      });
-
-      const payload = {
-        user: {
-          id: user.id,
-        },
-      };
-      jwt.sign(
-        payload,
-        config.get("jwtSecret"),
-        { expiresIn: 360000 },
-        (err, token) => {
-          if (err) throw err;
-          res.json({ token });
-        }
-      );
+      
     } catch (err) {
       console.error(err.message);
       res.status(500).send("Server error");
