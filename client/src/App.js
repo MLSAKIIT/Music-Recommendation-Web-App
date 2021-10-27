edc4b10e8402ac0bf8e5febafdeea364440bedfa
Tishasoumya-02 <tishasoumya@gmail.com>
2021-10-27 21:29:42 +0530

    Removed private route for time being

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ import { loadUser } from "./actions/auth";
 import setAuthToken from "./utils/setAuthToken";
 import artists from './components/Artists-Selection/Artists';
 import LoginPage from './components/SawoLogIn/SawoLogIn';
-import PrivateRoute from './auth/private-route';
+// import PrivateRoute from './auth/private-route';
 import Library from './components/Library/Library';
 
 if (localStorage.token) {
@@ -35,10 +35,10 @@ const App = () => {
           <Route  exact path="/" component={login} />
           <Route  exact path="/register" component={register} />
           <Route  exact path="/loginSawo" component={LoginPage} />
-          <PrivateRoute component={artists} path="/artists" auth={localStorage.token} />   
+          <Route component={artists} path="/artists"  />   
           <Route exact path="/library" component={Library} />  
           <Route  path="/loginSawo/choose-action" component={ChooseAction} />
-          <PrivateRoute  path="/music" component={Music} auth={localStorage.token} />
+          <Route  path="/music" component={Music}  />
         </Switch>
       </BrowserRouter>
     </Provider>
