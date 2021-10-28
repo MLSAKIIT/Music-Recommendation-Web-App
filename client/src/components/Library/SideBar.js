7baca007ae4674eb84b05507670adac5f85c6398
Tishasoumya-02 <tishasoumya@gmail.com>
2021-10-29 00:02:30 +0530

    Added side bar and search bar still needs to be worked upon will be done in due time

--- client/src/components/Library/SideBar.js
+++ client/src/components/Library/SideBar.js
@@ -1,14 +1,27 @@
 import classes from "./SideBar.module.css";
+import Divider from '@mui/material/Divider';
+
 
 const SideBar = () => {
   return (
     <div className={classes.sideBar}>
       <span>Library</span>
       <div className={classes.content}>
-        <a href="/home">Home</a>
+    
+        <ul>
+          <li><a href="/home">Home</a></li>
+          <Divider />
+          <li><a href="/playlist">Playlist</a></li>
+          <Divider />
+          <li> <a href="/artists">Artist</a></li>
+          <Divider   />
+          <li><a href="/favourite">Favourite</a> </li>
+          <Divider   />
+        </ul>
+        {/* <a href="/home">Home</a>
         <a href="/playlist">Playlist</a>
         <a href="/artists">Artist</a>
-        <a href="/favourite">Favourite</a>
+        <a href="/favourite">Favourite</a> */}
       </div>
     </div>
   );
